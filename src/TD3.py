46e6bf1b694df3a834a47d9cd4c9f6ec41cae0d4
Aakaash Salvaji <68838552+asal512@users.noreply.github.com>
2022-11-09 15:00:02 +1300

    Remove Model Based Code in Main TD3 file for testing

--- src/TD3.py
+++ src/TD3.py
@@ -48,40 +48,23 @@ class TD3_Agent:
         self.tau        = 0.005
         self.batch_size = 32
 
-        self.M = 300
         self.G = 10
         self.update_counter = 0
         self.policy_freq_update = 2
 
         self.max_memory_size_env   = 20_000
-        self.max_memory_size_model = 50_000
 
         self.actor_learning_rate      = 1e-4
         self.critic_learning_rate     = 1e-3
-        self.transition_learning_rate = 1e-3
 
         self.hidden_size_critic = [64, 64, 32]
         self.hidden_size_actor  = [64, 64, 32]
 
-        self.hidden_size_network_model  = [256, 128, 64]
-
         self.num_states  = 12
         self.num_actions = 1
-        #only used for model based
-        self.num_states_training = 14  # 16 in total but remove the target point from the state for training trans model
-
-        self.loss_transition_model_1 = []
-        self.loss_transition_model_2 = []
-        self.loss_transition_model_3 = []
-        self.loss_transition_model_4 = []
-        self.loss_transition_model_5 = []
-
-        self.epsilon       = 1
-        self.epsilon_min   = 0.001
-        self.epsilon_decay = 0.0008
 
         # ------------- Initialization memory --------------------- #
-        self.memory = MemoryClass(self.max_memory_size_env, self.max_memory_size_model)
+        self.memory = MemoryClass(self.max_memory_size_env)
 
         # ---------- Initialization and build the networks for TD3----------- #
         # Main networks
@@ -111,29 +94,6 @@ class TD3_Agent:
         self.critic_optimizer_2 = optim.Adam(self.critic_q2.parameters(), lr=self.critic_learning_rate)
 
 
-        # ---------- Initialization and build the networks for Model Learning ----------- #
-        self.pdf_transition_model_1 = ModelNet_probabilistic_transition(self.num_states_training + self.num_actions,
-                                                                        self.hidden_size_network_model)
-        self.pdf_transition_model_2 = ModelNet_probabilistic_transition(self.num_states_training + self.num_actions,
-                                                                        self.hidden_size_network_model)
-        self.pdf_transition_model_3 = ModelNet_probabilistic_transition(self.num_states_training + self.num_actions,
-                                                                        self.hidden_size_network_model)
-        self.pdf_transition_model_4 = ModelNet_probabilistic_transition(self.num_states_training + self.num_actions,
-                                                                        self.hidden_size_network_model)
-        self.pdf_transition_model_5 = ModelNet_probabilistic_transition(self.num_states_training + self.num_actions,
-                                                                        self.hidden_size_network_model)
-
-        self.pdf_transition_1_optimizer = optim.Adam(self.pdf_transition_model_1.parameters(),
-                                                     lr=self.transition_learning_rate, )
-        self.pdf_transition_2_optimizer = optim.Adam(self.pdf_transition_model_2.parameters(),
-                                                     lr=self.transition_learning_rate)
-        self.pdf_transition_3_optimizer = optim.Adam(self.pdf_transition_model_3.parameters(),
-                                                     lr=self.transition_learning_rate)
-        self.pdf_transition_4_optimizer = optim.Adam(self.pdf_transition_model_4.parameters(),
-                                                     lr=self.transition_learning_rate)
-        self.pdf_transition_5_optimizer = optim.Adam(self.pdf_transition_model_5.parameters(),
-                                                     lr=self.transition_learning_rate)
-
     def get_action_from_policy(self, state):
         state_tensor = torch.from_numpy(state).float().unsqueeze(0)
         self.actor.eval()
@@ -148,46 +108,19 @@ class TD3_Agent:
     def add_real_experience_memory(self, state, action, reward, next_state, done):
         self.memory.replay_buffer_environment_add(state, action, reward, next_state, done)
 
-    def add_imagined_experience_memory(self, state, action, reward, next_state, done):
-        self.memory.replay_buffer_model_add(state, action, reward, next_state, done)
+    def step_training(self):
+        
+        if self.memory.len_env_buffer() <= self.batch_size:
+            return
+        else:
+            self.update_weights()
 
-    def epsilon_greedy_function_update(self):
-        # this is used for choose the sample from memory when dream samples are generated
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= (1 - self.epsilon_decay)
-
-
-    def step_training(self, style="MFRL"):
-        if style == "MFRL":
-            if self.memory.len_env_buffer() <= self.batch_size:
-                return
-            else:
-                self.update_weights(style)
-
-        elif style == "MBRL":
-            if self.memory.len_env_buffer() and self.memory.len_model_buffer() <= self.batch_size:
-                return
-            else:
-                self.epsilon_greedy_function_update()
-                self.update_weights(style)
-
-
-    def update_weights(self, style):
+    def update_weights(self):
 
         for it in range(1, self.G + 1):
             self.update_counter += 1
-            if style == "MFRL":
-                states, actions, rewards, next_states, dones = self.memory.sample_experience_from_env(self.batch_size)
 
-            elif style == "MBRL":
-                # I did this because the model predict bad things at the beginning, so I choose and train the policy
-                # from previous real experiences and then move to the "dream" experiences using epsilon greedy.
-                if np.random.random() <= self.epsilon:
-                    states, actions, rewards, next_states, dones = self.memory.sample_experience_from_env(self.batch_size)
-                    print("sample from env data set")
-                else:
-                    print("sample from model data set")
-                    states, actions, rewards, next_states, dones = self.memory.sample_experience_from_model(self.batch_size)
+            states, actions, rewards, next_states, dones = self.memory.sample_experience_from_env(self.batch_size)
 
             states  = np.array(states)
             actions = np.array(actions)
@@ -255,202 +188,6 @@ class TD3_Agent:
                     target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
 
 
-    def transition_model_learn(self):
-        if self.memory.len_env_buffer() <= self.batch_size:
-            return
-        else:
-            input_state_minibatch  = []
-            output_state_minibatch = []
-
-            states, actions, rewards, next_states, dones = self.memory.sample_experience_from_env(self.batch_size)
-            # these values for states and next states include the target point but need to be removed
-
-            states  = np.array(states)
-            actions = np.array(actions)
-            rewards = np.array(rewards).reshape(-1, 1)
-            dones   = np.array(dones).reshape(-1, 1)
-            next_states = np.array(next_states)
-
-            # Remove the target point from state and next state because there is no point in predict
-            # the target value since it randomly changes in the environment.
-
-            for state in states:
-                input_state = state[:-2]
-                input_state_minibatch.append(input_state)
-
-            for next_state in next_states:
-                output_state = next_state[:-2]
-                output_state_minibatch.append(output_state)
-
-            states      = np.array(input_state_minibatch)
-            next_states = np.array(output_state_minibatch)
-
-            states  = torch.FloatTensor(states)
-            actions = torch.FloatTensor(actions)
-            rewards = torch.FloatTensor(rewards)
-            dones   = torch.FloatTensor(dones)
-            next_states = torch.FloatTensor(next_states)
-
-            # ---- Transition Model---- #
-            distribution_probability_model_1 = self.pdf_transition_model_1.forward(states, actions)
-            distribution_probability_model_2 = self.pdf_transition_model_2.forward(states, actions)
-            distribution_probability_model_3 = self.pdf_transition_model_3.forward(states, actions)
-            distribution_probability_model_4 = self.pdf_transition_model_4.forward(states, actions)
-            distribution_probability_model_5 = self.pdf_transition_model_5.forward(states, actions)
-
-            # calculate the loss
-            loss_neg_log_likelihood_1 = - distribution_probability_model_1.log_prob(next_states)
-            loss_neg_log_likelihood_2 = - distribution_probability_model_2.log_prob(next_states)
-            loss_neg_log_likelihood_3 = - distribution_probability_model_3.log_prob(next_states)
-            loss_neg_log_likelihood_4 = - distribution_probability_model_4.log_prob(next_states)
-            loss_neg_log_likelihood_5 = - distribution_probability_model_5.log_prob(next_states)
-
-            loss_neg_log_likelihood_1 = torch.mean(loss_neg_log_likelihood_1)
-            loss_neg_log_likelihood_2 = torch.mean(loss_neg_log_likelihood_2)
-            loss_neg_log_likelihood_3 = torch.mean(loss_neg_log_likelihood_3)
-            loss_neg_log_likelihood_4 = torch.mean(loss_neg_log_likelihood_4)
-            loss_neg_log_likelihood_5 = torch.mean(loss_neg_log_likelihood_5)
-
-            self.pdf_transition_model_1.train()
-            self.pdf_transition_1_optimizer.zero_grad()
-            loss_neg_log_likelihood_1.backward()
-            self.pdf_transition_1_optimizer.step()
-
-            self.pdf_transition_model_2.train()
-            self.pdf_transition_2_optimizer.zero_grad()
-            loss_neg_log_likelihood_2.backward()
-            self.pdf_transition_2_optimizer.step()
-
-            self.pdf_transition_model_3.train()
-            self.pdf_transition_3_optimizer.zero_grad()
-            loss_neg_log_likelihood_3.backward()
-            self.pdf_transition_3_optimizer.step()
-
-            self.pdf_transition_model_4.train()
-            self.pdf_transition_4_optimizer.zero_grad()
-            loss_neg_log_likelihood_4.backward()
-            self.pdf_transition_4_optimizer.step()
-
-            self.pdf_transition_model_5.train()
-            self.pdf_transition_5_optimizer.zero_grad()
-            loss_neg_log_likelihood_5.backward()
-            self.pdf_transition_5_optimizer.step()
-
-            self.loss_transition_model_1.append(loss_neg_log_likelihood_1.item())
-            self.loss_transition_model_2.append(loss_neg_log_likelihood_2.item())
-            self.loss_transition_model_3.append(loss_neg_log_likelihood_3.item())
-            self.loss_transition_model_4.append(loss_neg_log_likelihood_4.item())
-            self.loss_transition_model_5.append(loss_neg_log_likelihood_5.item())
-
-            print("Loss:", loss_neg_log_likelihood_1.item(), loss_neg_log_likelihood_2.item(),
-                           loss_neg_log_likelihood_3.item(), loss_neg_log_likelihood_4.item(),
-                           loss_neg_log_likelihood_5.item())
-
-
-    def generate_dream_samples(self):
-        if self.memory.len_env_buffer() <= self.batch_size:
-            return
-        else:
-            sample_batch = 1  # Fix to be =1 always
-            for _ in range(1, self.M + 1):
-                state, _, _, _, _ = self.memory.sample_experience_from_env(sample_batch)
-                state = np.array(state)
-                state = state[0]  # 16 values
-                target_point = state[-2:]  # just target point only
-
-                # Remove the target point from state
-                state_input  = state[:-2]  # 14 values
-                state_tensor = torch.FloatTensor(state_input)
-                state_tensor = state_tensor.unsqueeze(0)  # torch.Size([1, 14])
-
-                action = self.get_action_from_policy(state)  # array  of (4,)
-                action = np.clip(action, -1, 1)
-                noise  = (np.random.normal(0, scale=0.15, size=4))
-                action = action + noise
-                action = np.clip(action, -1, 1)
-
-                action_tensor = torch.FloatTensor(action)
-                action_tensor = action_tensor.unsqueeze(0)  # torch.Size([1, 4])
-
-                self.pdf_transition_model_1.eval()
-                with torch.no_grad():
-                    function_generated_1    = self.pdf_transition_model_1.forward(state_tensor, action_tensor)
-                    observation_generated_1 = function_generated_1.sample((14,))  # torch.Size([14, 1, 1])
-                    observation_generated_1 = torch.reshape(observation_generated_1, (1, 14))  # torch.Size([1, 14])
-                    observation_generated_1 = observation_generated_1.detach()
-                    observation_generated_1 = observation_generated_1.numpy()
-                    self.pdf_transition_model_1.train()
-
-                self.pdf_transition_model_2.eval()
-                with torch.no_grad():
-                    function_generated_2    = self.pdf_transition_model_2.forward(state_tensor, action_tensor)
-                    observation_generated_2 = function_generated_2.sample((14,))  # torch.Size([14, 1, 1])
-                    observation_generated_2 = torch.reshape(observation_generated_2, (1, 14))  # torch.Size([1, 14])
-                    observation_generated_2 = observation_generated_2.detach()
-                    observation_generated_2 = observation_generated_2.numpy()
-                    self.pdf_transition_model_2.train()
-
-                self.pdf_transition_model_3.eval()
-                with torch.no_grad():
-                    function_generated_3    = self.pdf_transition_model_3.forward(state_tensor, action_tensor)
-                    observation_generated_3 = function_generated_3.sample((14,))  # torch.Size([14, 1, 1])
-                    observation_generated_3 = torch.reshape(observation_generated_3, (1, 14))  # torch.Size([1, 14])
-                    observation_generated_3 = observation_generated_3.detach()
-                    observation_generated_3 = observation_generated_3.numpy()
-                    self.pdf_transition_model_3.train()
-
-                self.pdf_transition_model_4.eval()
-                with torch.no_grad():
-                    function_generated_4    = self.pdf_transition_model_4.forward(state_tensor, action_tensor)
-                    observation_generated_4 = function_generated_4.sample((14,))  # torch.Size([14, 1, 1])
-                    observation_generated_4 = torch.reshape(observation_generated_4, (1, 14))  # torch.Size([1, 14])
-                    observation_generated_4 = observation_generated_4.detach()
-                    observation_generated_4 = observation_generated_4.numpy()
-                    self.pdf_transition_model_4.train()
-
-                self.pdf_transition_model_5.eval()
-                with torch.no_grad():
-                    function_generated_5    = self.pdf_transition_model_5.forward(state_tensor, action_tensor)
-                    observation_generated_5 = function_generated_5.sample((14,))  # torch.Size([14, 1, 1])
-                    observation_generated_5 = torch.reshape(observation_generated_5, (1, 14))  # torch.Size([1, 14])
-                    observation_generated_5 = observation_generated_5.detach()
-                    observation_generated_5 = observation_generated_5.numpy()
-                    self.pdf_transition_model_5.train()
-
-
-                model_choose = random.randint(1, 5)
-                if model_choose   == 1:
-                    next_state_imagined = observation_generated_1[0]
-                elif model_choose == 2:
-                    next_state_imagined = observation_generated_2[0]
-                elif model_choose == 3:
-                    next_state_imagined = observation_generated_3[0]
-                elif model_choose == 4:
-                    next_state_imagined = observation_generated_4[0]
-                elif model_choose == 5:
-                    next_state_imagined = observation_generated_5[0]
-
-                # calculate the reward and distance based on the prediction and input not using a model-reward
-                cube_position   = next_state_imagined[-2:]
-                target_position = target_point
-
-                imagined_distance_cube_goal = np.linalg.norm(cube_position - target_position)  # millimeters distance
-
-                if imagined_distance_cube_goal <= 10:  # millimeters
-                    done = True
-                    reward_d = 100
-                else:
-                    done = False
-                    reward_d = -imagined_distance_cube_goal
-
-                # to complete the 16 values of the original state-space
-                next_state = np.append(next_state_imagined, target_position)
-
-                # add imagines experiences to model_imagined buffer
-                self.add_imagined_experience_memory(state, action, reward_d, next_state, done)
-
-
-
 #Function to allow exploration Noise = track noise in exploration
 def run_exploration(env, episodes, steps, agent, noise=False):
     for episode in range(1, episodes+1):
@@ -481,7 +218,7 @@ def run_exploration(env, episodes, steps, agent, noise=False):
     print(f"******* -----{episodes} for exploration ended-----********* ")
 
 #Function to train TD3 Model, track_noise = track noise in training
-def run_training(env, episodes, steps, agent, style, folder=FOLDER, track_noise=False):
+def run_training(env, episodes, steps, agent, folder=FOLDER, track_noise=False):
 
     rewards     = []
     best_reward = 0
@@ -537,16 +274,8 @@ def run_training(env, episodes, steps, agent, style, folder=FOLDER, track_noise=
             robot_x.append(x)
             robot_y.append(y)
             
-            # learn
-            if style == "MFRL":
-                agent.step_training(style)
+            agent.step_training()
 
-            elif style == "MBRL": 
-                agent.transition_model_learn()
-                agent.generate_dream_samples()
-                agent.step_training(style)
-            
-            
             if done or step == steps:
                 
                 # Store best episode (highest reward) model (overwritten)
@@ -605,7 +334,7 @@ def run_training(env, episodes, steps, agent, style, folder=FOLDER, track_noise=
 
     
 #Function to test TD3 Model, Noise = track noise in testing
-def run_testing(env, episodes_test, steps, agent, style, track, folder=FOLDER, noise=False):
+def run_testing(env, episodes_test, steps, agent, track, folder=FOLDER, noise=False):
 
     # Load model 5000 from folder created in training (CAN CHANGE EPISODE TO WHATEVER YOU WANT)
     model_episode = 5000
@@ -691,15 +420,13 @@ def WriteTestRewardsToFile(folder, reward, avg_error, completion, seg1, seg2, st
 def train(num_exploration_episodes=EXPLORATION_EPISODES,num_training_episodes=EXPLORATION_EPISODES,steps=STEPS,folder=FOLDER,noise=False):
     env   = TD3_Env()
     agent = TD3_Agent(env)
-    style = "MFRL" # model free RL
     run_exploration(env, num_exploration_episodes, steps, agent, noise)
-    run_training(env, num_training_episodes, steps, agent, style, folder, noise)
+    run_training(env, num_training_episodes, steps, agent, folder, noise)
 
 def test(episodes,steps,folder,noise,track):
     env   = TD3_Env()
     agent = TD3_Agent(env)
-    style = "MFRL" # model free RL
-    run_testing(env, episodes, steps, agent, style, track, folder, noise)
+    run_testing(env, episodes, steps, agent, track, folder, noise)
 
 
 
